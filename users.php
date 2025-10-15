e41352fc75938f1b85669e5b8684a993ef66f743
Zenixu <maybe4zen@gmail.com>
2025-10-15 10:39:44 +0700

    menambahkan fitur ganti password user

--- users.php
+++ users.php
@@ -22,6 +22,20 @@ include_once('templates/header.php');
   </div>
   <?php
       }
+    } else if (isset($_POST['ganti_password'])){
+        if(ganti_password($_POST) > 0){
+  ?>
+  <div class="alert alert-success" role="alert">
+    Password berhasil diubah!
+  </div>
+  <?php
+      } else {
+  ?>
+  <div class="alert alert-danger" role="alert">
+    Password gagal diubah!
+  </div>
+  <?php
+      }
     }
   ?>
 </div>
@@ -43,7 +57,7 @@ include_once('templates/header.php');
             <th class="text-center">No</th>
             <th class="text-center">Username</th>
             <th class="text-center">User Role</th>
-            <th colspan="2" class="text-center">Aksi</th>
+            <th colspan="3" class="text-center">Aksi</th>
           </tr>
         </thead>
         <!-- <tfoot>
@@ -68,10 +82,15 @@ include_once('templates/header.php');
             <td class="text-center"><?= $no++?></td>
             <td><?= $user['username']?></td>
             <td><?= $user['user_role']?></td>
+            <td>
+              <button type="button" class="btn btn-info btn-icon-split" data-toggle="modal" data-target="#gantiPassword" data-id="<?= $user['id_user']?>">
+                <span class="text">Ganti Password</span>
+              </button>
+            </td>
             <td class="text-center">
               <a class="btn btn-success" href="edit-user.php?id=<?= $user['id_user']?>">Ubah</a>
             </td>
-            <td class="text-center">
+            <td class="text-center">  
               <a href="hapus-user.php?id=<?= $user['id_user']?>" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')" class="btn btn-danger">Hapus</a>
             </td>
           </tr>
@@ -97,6 +116,35 @@ $urutan++;
 $huruf = "usr";
 $kodeuser = $huruf . sprintf("%02s", $urutan);
 ?>
+
+<!-- Modal Ganti Password -->
+<div class="modal fade" id="gantiPassword" tabindex="-1" aria-labelledby="gantiPasswordLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="gantiPasswordLabel">Ganti Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+          <input type="hidden" name="id_user" id="id_user">
+          <div class="form-group row">
+            <label for="password" class="col-sm-4 col-torn-label">Password Baru</label>
+            <div class="col-sm-7">
+              <input type="password" class="form-control" id="password" name="password">
+            </div>
+          </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Keluar</button>
+        <button type="submit" name="ganti_password" class="btn btn-primary">Simpan</button>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
 
 <!-- Modal -->
 <div class="modal fade" id="tambahModal" tabindex="-1" aria-labelledby="tambahModalLabel" aria-hidden="true">
